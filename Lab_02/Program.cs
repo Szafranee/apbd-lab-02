b1c66c95426ffe421118e7b89b3269f5550e8d35
Kacper Szafrański <kacperr.szafranski@gmail.com>
2024-03-08 13:07:53 +0100

    Add avg method

--- Lab_02/Program.cs
+++ Lab_02/Program.cs
@@ -2,8 +2,15 @@
 
 Console.WriteLine("Hello, Cruel World!!");
 
-for (var i = 0; i < 10; i++)
-{
-    Console.WriteLine(i);
-}
+Console.WriteLine(GetAvg([1,2,3,4,5]));
 
+static double GetAvg(int[] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        sum += arr[i];
+    }
+
+    return sum / arr.Length;
+}
