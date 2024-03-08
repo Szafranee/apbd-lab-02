ae2845e0f55e6a041573a6b5461ec5e754a1c105
Kacper Szafrański <kacperr.szafranski@gmail.com>
2024-03-08 13:32:36 +0100

    Changed sum to total in GetAvg

--- Lab_02/Program.cs
+++ Lab_02/Program.cs
@@ -8,13 +8,13 @@ Console.WriteLine(GetMax([1, 2, 3, 4, 5]));
 
 static double GetAvg(int[] arr)
 {
-    int sum = 0;
+    int total = 0;
     for (int i = 0; i < arr.Length; i++)
     {
-        sum += arr[i];
+        total += arr[i];
     }
 
-    return sum / arr.Length;
+    return total / arr.Length;
 }
 
 static int GetMax(int[] arr)
